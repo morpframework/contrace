f99cd0b5451e8bd5216e904dba6df70ef3d1d300
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-03-27 22:24:14 +0800

    remove default index view

--- contrace/app.py
+++ contrace/app.py
@@ -22,14 +22,6 @@ def get_approot(request):
     return AppRoot(request)
 
 
-@App.html(model=AppRoot, template='contrace/index.pt',
-          permission=ccperm.ViewHome)
-def index(context, request):
-    return {
-        "message": "Hello world"
-    }
-
-
 @App.template_directory()
 def get_template_directory():
     return 'templates'
