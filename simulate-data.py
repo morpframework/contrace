166a525ffe11ec1a790129b811c07ed1ea1229c4
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-03-29 12:39:53 +0800

    a bit improvement on sim performance and added matplotlib animation

--- simulate-data.py
+++ simulate-data.py
@@ -2,23 +2,41 @@ import random
 import time
 import os
 import json
+import sys
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.animation import writers
 
 WORLD = {
-    'width': 30,
-    'height': 30
+    'width': 100,
+    'height': 100
 }
 
-POPULATION = 40
-CYCLES = 100
-CELLSIZE = 5
+POPULATION = 2500
+CELLSIZE = 10
+INFECT_RADIUS = 1
 
-def tower_log(payload, filename='tower_log.txt'):
-    with open(filename,'a') as f:
-        f.write(json.dumps(payload) + '\n')
+TOWER_LOG = []
+INFECT_LOG = []
+CYCLES=10000
 
-def infect_log(payload, filename='infect_log.txt'):
-    with open(filename,'a') as f:
-        f.write(json.dumps(payload) + '\n')
+def tower_log(payload):
+    TOWER_LOG.append(payload)
+
+
+def infect_log(payload):
+    INFECT_LOG.append(payload)
+
+def basealpha(num):
+    num = int(num)
+    res = ''
+    up = int(num / 26)
+    bal = num % 26
+    if up:
+        res += basealpha(up - 1)
+    res += chr(bal + 65)
+    return res
+
 
 class Area(object):
 
@@ -42,8 +60,9 @@ class Area(object):
 
 class CellTower(Area):
     
-    def __init__(self, cellid, *args, **kwargs):
+    def __init__(self, cellid, world, *args, **kwargs):
         self.id = cellid
+        self.world = world
         super().__init__(*args, **kwargs)
         self.people = []
 
@@ -54,12 +73,12 @@ class CellTower(Area):
 
         if incell and person not in self.people:
             self.people.append(person)
-            tower_log({'timestamp': int(time.time()), 'cell_id': self.id, 'event':
+            tower_log({'timestamp': self.world.time, 'cell_id': self.id, 'event':
                 'Enter Cell', 'person_id': person.id})
 
         if not incell and person in self.people:
             self.people.remove(person)
-            tower_log({'timestamp': int(time.time()), 'cell_id': self.id, 'event':
+            tower_log({'timestamp': self.world.time, 'cell_id': self.id, 'event':
                 'Leave Cell', 'person_id': person.id})
 
 
@@ -71,14 +90,22 @@ class World(object):
     def __init__(self, width=WORLD['width'], height=WORLD['height']):
         self.width = width
         self.height = height
+        self.index = {}
+        for x in range(width):
+            for y in range(height):
+                self.index[(x,y)] = []
+
+        self.y_index = {}
         self.people = []
         self.towers = []
-        cidx = 65
+        self.time = 0
+        self.infected_count = 1
+        cidx = 0
         for x in range(0, width, CELLSIZE):
             cidy = 0
             for y in range(0, height, CELLSIZE):
-                cell_id = '{}{}'.format(chr(cidx),cidy)
-                tower = CellTower(cell_id, x, y, x+10, y+10)
+                cell_id = '{}-{}'.format(basealpha(cidx),cidy)
+                tower = CellTower(cell_id, self, x, y, x+10, y+10)
                 cidy += 1
                 self.towers.append(tower)
             cidx += 1
@@ -86,6 +113,8 @@ class World(object):
     def add_person(self, person):
         person.world = self
         self.people.append(person)
+        if person not in self.index[(person.x, person.y)]:
+            self.index[(person.x,person.y)].append(person)
         self.notify_tower(person)
 
     def find_people_in_area(self, x1, y1, x2, y2):
@@ -98,95 +127,86 @@ class World(object):
            
     def tick(self):
         for person in self.people:
+            if person in self.index[(person.x, person.y)]:
+                self.index[(person.x, person.y)].remove(person)
             person.tick()
+            if person not in self.index[(person.x, person.y)]:
+                self.index[person.x,person.y].append(person)
+        self.time += 1
 
-    def display(self):
-        infected = list([i for i in self.people if
-            i.infected])
-        infected_count = len(infected)
-        population_count = len(self.people)
-        clean_count = population_count - infected_count
-        print('Population = %s' % population_count)
-        print('Infected = %s' % infected_count)
-        print('Clean = %s' % clean_count)
-        for y in range(-1,self.height+2):
-            for x in range(-1,self.width+2):
-                if y == -1 and x == -1:
-                    print('+', end='')
-                if y == -1 and x > -1:
-                    print('+', end='')
-                if y > -1 and x == -1:
-                    print('+', end='')
-                if y == -1:
-                    continue
-                if x == -1:
-                    continue
-
-
-                if y == self.height+1 and x == self.width+1:
-                    print('+',end='')
-                    continue
-
-                if y == self.height+1 and x < self.width+1:
-                    print('+', end='')
-                    continue
-
-                if y <= self.height+1 and x == self.width+1:
-                    print('+', end='')
-                    continue
+    def scatter_vectors(self):
+        clean_x = []
+        clean_y = []
+        infected_x = []
+        infected_y = []
+        for p in self.people:
+            if not p.infected:
+                clean_x.append(p.x)
+                clean_y.append(p.y)
+            else:
+                infected_x.append(p.x)
+                infected_y.append(p.y)
+        return {
+            'infected': {
+                'x': infected_x,
+                'y': infected_y,
+                's': 1,
+                'color': 'r',
+            },
+            'clean': {
+                'x': clean_x,
+                'y': clean_y,
+                's': 1,
+                'color': 'b'
+            }
+        }
 
 
-
-                people_in_location = []
-                for person in self.people:
-                    person.infect(self)
-                    if person.x == x and person.y == y:
-                        people_in_location.append(person)
-                if people_in_location:
-                    has_infected = False
-                    for p in people_in_location:
-                        if p.infected:
-                            has_infected = True
-                    if has_infected:
-                        print('Z', end='')
-                    else:
-                        print('X', end='')
-                else:
-                    print(' ', end='')
-            print('')
-        
 class Person(object):
 
     def __init__(self, personid):
         self.id = personid
         self.world = None
-        self.x = random.randint(0, WORLD['width'])
-        self.y = random.randint(0, WORLD['height'])
+        self.x = random.randint(0, WORLD['width'] - 1)
+        self.y = random.randint(0, WORLD['height'] - 1)
         self.infected = False
         self.vector = {'x':random.randint(-1,1), 'y': random.randint(-1,1)}
+        self.steps = 0
         self.update_vector()
 
     def update_vector(self):
+        
+        self.vector['x'] = random.randint(-1,1)
+        self.vector['y'] = random.randint(-1,1)
+
         nextx = self.x + self.vector['x']
         nexty = self.y + self.vector['y']
+
+        # corner tracking
         if nextx < 0:
             self.vector['x'] = random.randint(0,1)
-        elif nextx > WORLD['height']:
+        elif nextx >= WORLD['height']:
             self.vector['x'] = random.randint(-1,0)
         
         if nexty < 0:
             self.vector['y'] = random.randint(0,1)
-        elif nexty > WORLD['width']:
+        elif nexty >= WORLD['width']:
             self.vector['y'] = random.randint(-1,0)
 
+        # must always be moving
         if self.vector['x'] == 0 and self.vector['y'] == 0:
-            # must always be moving
-            self.vector['x'] = random.sample([-1,1],1)[0]
-            self.vector['y'] = random.sample([-1,1],1)[0]
+            self.update_vector()            
+
+
+
+
 
     def tick(self):
+        self.infect(self.world)
         self.x += self.vector['x']
         self.y += self.vector['y']
+
+
         self.world.notify_tower(self)
         self.update_vector()
 
@@ -194,16 +214,73 @@ class Person(object):
         if not self.infected:
             return
 
-        for person in world.people:
-            if (((self.x - 1) <= person.x) and ((self.y - 1) <= person.y) and 
-                    (person.x <= (self.x + 1)) and (person.y <= (self.y + 1))):
-                if not person.infected:
-                    person.infected = True
-                    infect_log({'timestamp': int(time.time()), 'source_person':
-                        self.id, 'person': person.id})
+        for x in range((self.x - INFECT_RADIUS), (self.x + INFECT_RADIUS)):
+            for y in range((self.y - INFECT_RADIUS), (self.y + INFECT_RADIUS)):
+                people = world.index.get((x,y), [])
+
+                for person in people:
+
+                    if not person.infected:
+                        person.infected = True
+                        infect_log({'timestamp': world.time, 'source_person':
+                            self.id, 'person': person.id})
+                        world.infected_count += 1
+
+class ScatterAnimation(object):
+
+    def __init__(self, world):
+        self.fig = plt.figure()
+        self.world = world
+        self.ax = self.fig.add_axes([0, 0, 1, 1], frameon=False)
+        sv = world.scatter_vectors()
+        self.scats = []
+        for v in sv.values():
+            self.scats.append(self.ax.scatter(**v))
+        self.anim = FuncAnimation(self.fig, self.update, interval=100)
+        
+    def update(self, frame):
+        if self.world.infected_count >= POPULATION:
+            self.anim.event_source.stop()
+
+
+        global TOWER_LOG
+        global INFECT_LOG
+        print('Computing T = %s' % self.world.time)
+        self.world.tick()
+
+        for scat in self.scats:
+            scat.remove()
+
+        self.scats = []
+
+        sv = self.world.scatter_vectors()
+        for v in sv.values():
+            if v['x']:
+                self.scats.append(self.ax.scatter(**v))
+
+        with open('tower_log.txt','a') as f:
+            f.writelines([(json.dumps(l) + '\n') for l in TOWER_LOG])
+            TOWER_LOG=[]
+        with open('infect_log.txt','a') as f:
+            f.writelines([(json.dumps(l) + '\n') for l in INFECT_LOG])
+            INFECT_LOG=[]
+
+      
+
+
 
 
 def main():
+    global TOWER_LOG
+    global INFECT_LOG
+
+    if os.path.exists('tower_log.txt'):
+        os.unlink('tower_log.txt')
+        open('tower_log.txt','w').close()
+    if os.path.exists('infect_log.txt'):
+        os.unlink('infect_log.txt')
+        open('infect_log.txt','w').close()
+
     world = World()
     for p in range(POPULATION):
         person = Person(p)
@@ -211,14 +288,11 @@ def main():
             person.infected=True
         world.add_person(person)
 
-    for c in range(CYCLES):
-        world.tick()
-        os.system('clear')
-        print('T = %s' % c)
-        world.display()
-        if len(list([i for i in world.people if i.infected])) >= POPULATION:
-            break
-        time.sleep(1)
+    anim = ScatterAnimation(world)
+
+    plt.show()
+
+    
 
 
 if __name__ == '__main__':
